98ba2a738f6f05b0d6585722505d5f958198cdf3
aartaka <aartaka@protonmail.com>
2020-06-18 22:38:36 +0500

    Rearrange the groups of packages in the base-manifest

--- base-manifest.scm
+++ base-manifest.scm
@@ -3,12 +3,12 @@
    "clhs" ; NON-FREE
    ;; Lisp dependencies of my stumpwm. Somewhat awkward way to load deps. Meh.
    "sbcl-cl-ppcre" "sbcl-clx-truetype" "sbcl-ttf-fonts" "sbcl-zpng" "sbcl-dexador" "cl-slime-swank"
-   ;; Xorg/linux utilities for stumpwm
+   ;; Xorg utilities for stumpwm
    "xrdb" "xev" "xfontsel" "xmodmap" "xset" "xsetroot" "xinit" "xinput"
    ;; Scripting and everyday programming.
    "python" "sbcl"
    ;; CLI utils.
-   "p7zip" "dosfstools" "file" "tree" "unzip" "git" "sysfsutils"
+   "p7zip" "dosfstools" "file" "tree" "zip" "unzip" "git" "sysfsutils" "gnupg" "pinentry"
    "unrar" ; NON-FREE
    ;; Useful everyday programs.
    "icecat" "ungoogled-chromium" "libreoffice" "keepassxc" "rxvt-unicode" "pidgin" "vlc" "pavucontrol"))
