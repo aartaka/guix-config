89e39f0f65e577d47a48c72558e732e7777e29f1
aartaka <aartaka@protonmail.com>
2020-06-23 15:56:26 +0500

    base-manifest.scm: migrate back to SLY

--- base-manifest.scm
+++ base-manifest.scm
@@ -1,10 +1,11 @@
 (use-modules (gnu packages))
 
 (specifications->manifest
- '("emacs" "emacs-guix" "emacs-slime" "poppler" "emacs-pdf-tools"
+ '("emacs" "emacs-guix" "poppler" "emacs-pdf-tools" "ghostscript"
    "clhs" ; NON-FREE
    ;; Lisp dependencies of my stumpwm. Somewhat awkward way to load deps. Meh.
-   "sbcl-cl-ppcre" "sbcl-clx-truetype" "sbcl-ttf-fonts" "sbcl-zpng" "sbcl-dexador" "cl-slime-swank"
+   "sbcl-cl-ppcre" "sbcl-clx-truetype" "sbcl-zpng" "sbcl-dexador" "sbcl-slynk"
+   "stumpwm:lib" "sbcl-stumpwm-ttf-fonts"
    ;; Xorg utilities for stumpwm
    "xrdb" "xev" "xfontsel" "xmodmap" "xset" "xsetroot" "xinit" "xinput"
    ;; Scripting and everyday programming.
