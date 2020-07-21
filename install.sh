668b991d062d521c4e3469b35613e474697e8839
aartaka <aartaka@protonmail.com>
2020-07-21 19:10:47 +0500

    install.sh: Replace symlinks with copying and add emacs-manifest.scm

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
-ln -sf dev-manifest.scm ~/.dev-manifest.scm
-ln -sf channels.scm ~/.config/guix/channels.scm
-sudo ln -sf nonfree-desktop.scm /etc/nonfree-desktop.scm
+cp -f channels.scm ~/.config/guix/channels.scm
+sudo cp -f nonfree-desktop.scm /etc/nonfree-desktop.scm
+guix pull
+guix package -m base-manifest.scm --profile=$GUIX_EXTRA_PROFILES/base-profile/base-profile
+guix package -m emacs-manifest.scm --profile=$GUIX_EXTRA_PROFILES/emacs-profile/emacs-profile
+guix package -m dev-manifest.scm --profile=$GUIX_EXTRA_PROFILES/dev-profile/dev-profile
+guix package -m gimp-manifest.scm --profile=$GUIX_EXTRA_PROFILES/gimp-profile/gimp-profile
