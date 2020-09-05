14f4a30ff54675e0c3e8473315694a3e4b6163e4
aartaka <aartaka@protonmail.com>
2020-09-05 12:29:04 +0500

    channels.scm: add Chur-Guix to the list of channels

--- channels.scm
+++ channels.scm
@@ -1,5 +1,8 @@
 (cons*
-  (channel
-   (name 'nonguix)
-   (url "https://gitlab.com/nonguix/nonguix"))
+ (channel
+  (name 'nonguix)
+  (url "https://gitlab.com/nonguix/nonguix"))
+ (channel
+  (name 'chur)
+  (url "https://github.com/aartaka/chur-guix"))
  %default-channels)
