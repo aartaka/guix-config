e3b59efc534c48a8597c1874d8c1634184cc4c37
aartaka <aartaka@protonmail.com>
2020-10-11 00:39:45 +0500

    emacs-manifest: Return to Emacs-Guix.

--- emacs-manifest.scm
+++ emacs-manifest.scm
@@ -1,7 +1,7 @@
 (use-modules (gnu packages))
 
 (specifications->manifest
- '("emacs" "emacs-telega" "emacs-pdf-tools"
+ '("emacs" "emacs-telega" "emacs-pdf-tools" "emacs-guix"
    "ghostscript" "unoconv" "poppler" "imagemagick" "mupdf" "texlive" "ripgrep"
    "aspell" "aspell-dict-ru" "aspell-dict-en"
    ;; Non-free
