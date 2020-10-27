62a2c1f4ee54d0bd2dc9a3201743c6e9f520111d
aartaka <aartaka@protonmail.com>
2020-10-27 23:00:16 +0500

    emacs-manifest: Use package->manifest instead of specifications.

--- emacs-manifest.scm
+++ emacs-manifest.scm
@@ -1,8 +1,26 @@
-(use-modules (gnu packages))
+(use-modules (gnu packages)
+             (gnu packages aspell)
+             (gnu packages emacs)
+             (gnu packages emacs-xyz)
+             (gnu packages gnupg)
+             (gnu packages ghostscript)
+             (gnu packages imagemagick)
+             (gnu packages libreoffice)
+             (gnu packages lisp-xyz)
+             (gnu packages pdf)
+             (gnu packages python-xyz)
+             (gnu packages rust-apps)
+             (gnu packages tex)
 
-(specifications->manifest
- '("emacs" "emacs-telega" "emacs-pdf-tools" "emacs-guix"
-   "ghostscript" "unoconv" "poppler" "imagemagick" "mupdf" "ripgrep"
-   "aspell" "aspell-dict-ru" "aspell-dict-en"
-   ;; Non-free
-   "clhs"))
+             (guix packages)
+
+             (nongnu packages emacs))
+
+(packages->manifest
+ (list emacs
+       emacs-telega emacs-pinentry emacs-pdf-tools emacs-sly sbcl-slynk
+       ghostscript unoconv poppler imagemagick mupdf ripgrep pinentry
+       aspell aspell-dict-ru aspell-dict-en
+
+       ;; Non-free
+       clhs))
