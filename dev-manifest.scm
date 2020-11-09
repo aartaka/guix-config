afc525a7abc78d5d8e6246fe15f3b4a963cfcc88
aartaka <aartaka@protonmail.com>
2020-11-10 04:34:51 +0500

    dev-manifest: Use package->manifests instead of specifications.

--- dev-manifest.scm
+++ dev-manifest.scm
@@ -1,24 +1,27 @@
-(specifications->manifest
- '("llvm"
-   "clang"
-   "clang-runtime"
-   "clang-toolchain"
-   "glibc"
-   "gdb"
-   "gcc-toolchain"
-   "make"
-   "cmake"
-   "pkg-config"
-   "patchelf"
-   "coreutils"
-   "binutils"
-   "elfutils"
+(use-modules (gnu packages)
+             (gnu packages base)
+             (gnu packages commencement)
+             (gnu packages elf)
+             (gnu packages gdb)
+             (gnu packages guile)
+             (gnu packages jupyter)
+             (gnu packages llvm)
+             (gnu packages mono)
+             (gnu packages pkg-config)
+             (gnu packages python)
+             (gnu packages python-xyz)
+             (gnu packages scheme)
 
-   "mono"
+             (guix packages))
 
-   "python"
-   "jupyter"
-   "xeus"
-
-   "guile"
-   "racket"))
+(packages->manifest
+ (list llvm clang clang-runtime clang-toolchain
+       glibc gdb gcc-toolchain
+       make cmake pkg-config
+       patchelf coreutils binutils elfutils
+       ;; C#
+       mono
+       ;; Python & Jupyter
+       python jupyter xeus
+       ;; Lisps that are not CL
+       guile racket))
