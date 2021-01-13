3a615a7433964b9e6427959f0d3f060351be71a3
aartaka <aartaka@protonmail.com>
2021-01-13 17:10:15 +0500

    emacs-manifest: Add curl.

--- emacs-manifest.scm
+++ emacs-manifest.scm
@@ -1,5 +1,6 @@
 (use-modules (gnu packages)
              (gnu packages aspell)
+             (gnu packages curl)
              (gnu packages emacs)
              (gnu packages emacs-xyz)
              (gnu packages gnupg)
@@ -21,7 +22,7 @@
  (list emacs
        emacspeak espeak-ng
        emacs-telega emacs-pinentry emacs-pdf-tools emacs-sly cl-slynk
-       ghostscript unoconv poppler imagemagick mupdf ripgrep pinentry
+       curl ghostscript unoconv poppler imagemagick mupdf ripgrep pinentry
        aspell aspell-dict-ru aspell-dict-en
 
        ;; Non-free
