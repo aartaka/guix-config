e1009a001aee3ce639a269069bbc855ac94efc39
Artyom Bologov <aartaka@protonmail.com>
2021-01-29 06:05:43 +0500

    base-manifest: Add perl.
    It's required by some of advanced Magit tricks.

--- base-manifest.scm
+++ base-manifest.scm
@@ -13,9 +13,9 @@
    "bluez" "blueman"
    ;; Scripting and everyday programming.
    "python" "sbcl" "ccl"
-   ;; CLI utils.
+   ;; CLI utils and other necessary things.
    "p7zip" "dosfstools" "file" "tree" "zip" "unzip" "git" "sysfsutils"
-   "gnupg" "pinentry" "emacs-pinentry"
+   "gnupg" "pinentry" "emacs-pinentry" "perl"
    "unrar" ; NON-FREE
    ;; Useful everyday programs.
    "ungoogled-chromium" "icecat" "libreoffice" "keepassxc" "rxvt-unicode"
