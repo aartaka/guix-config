40c9bb352c1ece130582a2525d70ab0f6afe3fa8
Artyom Bologov <aartaka@protonmail.com>
2021-04-19 14:10:21 +0500

    nyxt-manifest: Leave only the foreign libs.

--- nyxt-manifest.scm
+++ nyxt-manifest.scm
@@ -10,46 +10,4 @@
     "qtbase"
     "qtwebengine"
     "qtwebchannel"
-    "qtdeclarative"
-
-    "cl-slynk"                          ; To hack Nyxt from the SLY REPL.
-
-    ;; Direct dependencies:
-    "cl-alexandria"
-    "cl-bordeaux-threads"
-    "cl-chanl"
-    "cl-css"
-    "cl-file-attributes"
-    "cl-json"
-    "cl-markup"
-    "cl-html-diff"
-    "cl-ppcre"
-    "cl-prevalence"
-    "cl-closer-mop"
-    "cl-containers"
-    "cl-cluffer"
-    "cl-dexador"
-    "cl-enchant"
-    "cl-fset"
-    "cl-hu.dwim.defclass-star"
-    "cl-iolib"
-    "cl-local-time"
-    "cl-log4cl"
-    "cl-mk-string-metrics"
-    "cl-moptilities"
-    "cl-osicat" ; Only Guix package of Nyxt needs SBCL version needed for libosicat.so.
-    "cl-parenscript"
-    "cl-plump"
-    "cl-quri"
-    "cl-serapeum"
-    "cl-str"
-    "cl-slime-swank"
-    "cl-trivia"
-    "cl-trivial-clipboard"
-    "cl-trivial-features"
-    "cl-trivial-package-local-nicknames"
-    "cl-trivial-types"
-    "cl-unix-opts"
-    ;; WebKitGTK deps
-    "cl-cffi-gtk" ; TODO: Does it have info doc?
-    "cl-webkit"))
+    "qtdeclarative"))
