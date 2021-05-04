fb5e1c62f9d8e59e58165fa85819c1dcdc3fb21f
Artyom Bologov <aartaka@protonmail.com>
2021-05-04 16:34:22 +0500

    nyxt-manifest: Add Nyxt native inputs.

--- nyxt-manifest.scm
+++ nyxt-manifest.scm
@@ -2,5 +2,8 @@
              (srfi srfi-1))
 
 (specifications->manifest
- (map (compose package-full-name second)
-      (package-inputs (load "/home/aartaka/git/nyxt/build-scripts/guix.scm"))))
+ (append
+  (map (compose package-full-name second)
+       (package-inputs (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))
+  (map (compose package-full-name second)
+      (package-native-inputs (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))))
