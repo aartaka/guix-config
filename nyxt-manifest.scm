cf5908a5163778595bc4cfd4e5ee50fd6f0b9108
Artyom Bologov <aartaka@protonmail.com>
2021-05-19 01:15:13 +0500

    nyxt-manifest: Prettify formatting.

--- nyxt-manifest.scm
+++ nyxt-manifest.scm
@@ -4,6 +4,8 @@
 (specifications->manifest
  (append
   (map (compose package-full-name second)
-       (package-inputs (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))
+       (package-inputs
+        (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))
   (map (compose package-full-name second)
-      (package-native-inputs (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))))
+       (package-native-inputs
+        (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))))
