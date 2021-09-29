f2166d399f173695c891e4111e4466239188b6cb
aartaka <aartaka@protonmail.com>
2021-09-29 09:58:01 +0500

    install.sh: Add GPG config & initial profile setup.

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+mkdir -p $HOME/.guix-extra-profiles
+GUIX_EXTRA_PROFILES=$HOME/.guix-extra-profiles
+
 cp -f bash-profile ~/.bash_profile
 cp -f xinitrc ~/.xinitrc
+mkdir -p ~/.gnupg
+cp -f gpg-agent-conf ~/.gnupg/gpg-agent.conf
 
 profile_names=("emacs" "dev" "gimp" "nyxt" "texlive")
 for NAME in ${profile_names[*]}; do
     echo "Installing $NAME profile..."
+    mkdir -p "$HOME/.guix-extra-profiles/$NAME-profile/"
     guix package -m "$NAME-manifest.scm" --profile="$HOME/.guix-extra-profiles/$NAME-profile/$NAME-profile"
 done
 
