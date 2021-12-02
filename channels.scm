099b4ad327f49a512650c618f84ff3b3e6cdd3cc
Artyom Bologov <aartaka@protonmail.com>
2021-12-02 22:01:58 +0500

    channels: Add gaming channels.

--- channels.scm
+++ channels.scm
@@ -1,14 +1,11 @@
 (cons*
  (channel
   (name 'nonguix)
-  (url "https://gitlab.com/nonguix/nonguix")
-  ;; Enable signature verification:
-  (introduction
-   (make-channel-introduction
-    "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-    (openpgp-fingerprint
-     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+  (url "https://gitlab.com/nonguix/nonguix"))
  (channel
   (name 'chur)
   (url "https://github.com/aartaka/chur-guix"))
+ (channel
+  (name 'guix-gaming-games)
+  (url "https://gitlab.com/guix-gaming-channels/games"))
  %default-channels)
