6fa559682c9a5a7ea3c75a85c7879859f9d6886d
aartaka <aartaka@protonmail.com>
2021-12-29 21:40:28 +0500

    channels: Add personal channel.

--- channels.scm
+++ channels.scm
@@ -6,6 +6,9 @@
   (name 'chur)
   (url "https://github.com/aartaka/chur-guix"))
  (channel
+  (name 'aartaka)
+  (url "https://github.com/aartaka/guix-channel"))
+ (channel
   (name 'guix-gaming-games)
   (url "https://gitlab.com/guix-gaming-channels/games"))
  %default-channels)
