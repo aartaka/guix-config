b2dc91b8dac7c3f36ea7e8ab72b36fca7814cf53
aartaka <aartaka@protonmail.com>
2022-02-08 13:23:54 +0500

    nyxt-manifest: Change the name of the manifest file.

--- nyxt-manifest.scm
+++ nyxt-manifest.scm
@@ -5,8 +5,8 @@
  (append
   (map (compose package-full-name second)
        (package-inputs
-        (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))
+        (load "/home/aartaka/git/nyxt/build-scripts/nyxt.scm")))
   (map (compose package-full-name second)
        (package-native-inputs
-        (load "/home/aartaka/git/nyxt/build-scripts/guix.scm")))
+        (load "/home/aartaka/git/nyxt/build-scripts/nyxt.scm")))
   (list "cl-osicat" "cl-markup")))
