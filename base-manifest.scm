19a53ea073e105f84ec5439af62488b1e4262661
aartaka <aartaka@protonmail.com>
2022-03-11 14:04:57 +0500

    base-manifest: Add cl-cffi.

--- base-manifest.scm
+++ base-manifest.scm
@@ -1,7 +1,7 @@
 (use-modules (gnu packages))
 
 (specifications->manifest
- '("cl-alexandria" "cl-ppcre" "cl-slynk" "cl-bordeaux-threads"
+ '("cl-alexandria" "cl-ppcre" "cl-slynk" "cl-bordeaux-threads" "cl-cffi"
    ;; Fonts
    "font-ibm-plex" "font-dejavu" "font-liberation"
    "font-abattis-cantarell" "font-wqy-microhei"
