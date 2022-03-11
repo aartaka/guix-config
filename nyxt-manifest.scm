ba32a7218d2388dcc2d5e6e31a474bbf3493ff95
aartaka <aartaka@protonmail.com>
2022-03-11 18:30:48 +0500

    nyxt-manifest: Use package-name instead of package-full-name.
    package-full-name sometimes asks for versions that are not yet
    available in Guix.

--- nyxt-manifest.scm
+++ nyxt-manifest.scm
@@ -3,10 +3,10 @@
 
 (specifications->manifest
  (append
-  (map (compose package-full-name second)
+  (map (compose package-name second)
        (package-inputs
         (load "/home/aartaka/git/nyxt/build-scripts/nyxt.scm")))
-  (map (compose package-full-name second)
+  (map (compose package-name second)
        (package-native-inputs
         (load "/home/aartaka/git/nyxt/build-scripts/nyxt.scm")))
   (list "cl-osicat" "cl-markup")))
