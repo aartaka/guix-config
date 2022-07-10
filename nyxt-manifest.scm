1396137d280ac6cbebd66c5a997cdce0c5cd68de
aartaka <aartaka@protonmail.com>
2022-07-10 22:38:45 +0400

    nyxt-manifest: Resolve nyxt path based on $HOME.

--- nyxt-manifest.scm
+++ nyxt-manifest.scm
@@ -5,8 +5,8 @@
  (append
   (map (compose package-name second)
        (package-inputs
-        (load "/home/aartaka/git/nyxt/build-scripts/nyxt.scm")))
+        (load (string-append (getenv "HOME") "/git/nyxt/build-scripts/nyxt.scm"))))
   (map (compose package-name second)
        (package-native-inputs
-        (load "/home/aartaka/git/nyxt/build-scripts/nyxt.scm")))
+        (load (string-append (getenv "HOME") "/git/nyxt/build-scripts/nyxt.scm"))))
   (list "cl-osicat" "cl-markup")))
