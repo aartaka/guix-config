e956287ef05f67c4159d3b940c1c71ce31192d7b
aartaka <aartaka@protonmail.com>
2022-09-15 12:32:08 +0400

    emacs-manifest: Add more dependencies.

--- emacs-manifest.scm
+++ emacs-manifest.scm
@@ -22,11 +22,42 @@
 
 (packages->manifest
  (list emacs
-       emacs-use-package
+       emacs-use-package emacs-diminish
+       emacs-all-the-icons
+       emacs-all-the-icons-dired emacs-all-the-icons-completion
+       emacs-miniedit
+       ;; Completion and snippets
+       emacs-company emacs-helm-company emacs-auto-complete
+       emacs-yasnippet emacs-yasnippet-snippets emacs-helm-c-yasnippet
+       emacs-eldoc
+       ;; Whatever
+       emacs-helm-emms
+       emacs-nov-el emacs-pdf-tools
+       emacs-erc-image
        emacs-magit
+       ;; Helm
+       emacs-helm-projectile emacs-helm-ag emacs-helm-swoop
+       ;; Org Mode
+       emacs-ox-gemini emacs-ox-pandoc emacs-org-ref
+       emacs-markdown-mode emacs-edit-indirect
        ;; emacspeak espeak-ng
        ;; emacs-telega
-       emacs-pinentry emacs-pdf-tools emacs-sly cl-slynk
+       emacs-pinentry
+       ;; Lisps
+       emacs-sly cl-slynk
+       emacs-sly-stepper emacs-sly-quicklisp
+       emacs-sly-named-readtables emacs-sly-macrostep emacs-sly-asdf
+       emacs-helm-sly emacs-helm-xref
+       emacs-geiser emacs-geiser-racket emacs-geiser-guile emacs-racket-mode
+       emacs-paredit emacs-tagedit
+       emacs-parseclj emacs-parseedn emacs-cider
+       ;; C/C++
+       emacs-nhexl-mode emacs-go-mode
+       ;; Python
+       emacs-elpy emacs-flycheck emacs-helm-flycheck
+       ;; Web (HTML/CSS/JS)
+       emacs-web-mode emacs-js2-mode emacs-rjsx-mode emacs-skewer-mode emacs-rainbow-mode
+       emacs-smart-mode-line
        mu offlineimap3
        curl ghostscript unoconv poppler imagemagick mupdf ripgrep pinentry
        aspell aspell-dict-ru aspell-dict-en
