fc607650f1dbd29936be694a4dc7fe59b89f9d5a
Artyom Bologov <aartaka@protonmail.com>
2022-11-22 23:21:03 +0400

    icecat-manifest: Add.

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ profile_names="$@"
 
 if [ 0 -eq $# ];
 then
-    profile_names=("emacs" "dev" "gimp" "sc" "texlive" "nyxt" "chromium" "firefox")
+    profile_names=("emacs" "dev" "gimp" "sc" "texlive" "nyxt" "chromium" "icecat" "firefox")
 fi
 
 for NAME in ${profile_names[*]}; do
