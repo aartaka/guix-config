2fd00ecf577db344de4bc1327f615c7d299494ff
Artyom Bologov <aartaka@protonmail.com>
2023-02-06 14:04:38 +0400

    nonfree-desktop: A saner Postgres setup.

--- nonfree-desktop.scm
+++ nonfree-desktop.scm
@@ -133,7 +133,16 @@
                   (service docker-service-type)
                   (service postgresql-service-type
                            (postgresql-configuration
-                            (postgresql postgresql)))
+                            (postgresql postgresql)
+                            (config-file
+                             (postgresql-config-file
+                              (log-destination "stderr")
+                              (hba-file
+                               (plain-file "pg_hba.conf"
+                                           "
+local	all	all			trust
+host	all	all	127.0.0.1/32 	md5
+host	all	all	::1/128 	md5"))))))
                  (service postgresql-role-service-type
                            (postgresql-role-configuration
                             (roles (list (postgresql-role
