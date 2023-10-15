95836904a2607e9754cd060c9a3d4ee11343e548
aartaka <aartaka@protonmail.com>
2023-10-15 21:29:21 +0400

    channels: Fix extra paren.

--- channels.scm
+++ channels.scm
@@ -30,5 +30,5 @@
    (make-channel-introduction
     "c23d64f1b8cc086659f8781b27ab6c7314c5cca5"
     (openpgp-fingerprint
-     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F")))))
+     "50F3 3E2E 5B0C 3D90 0424  ABE8 9BDC F497 A4BB CC7F"))))
  %default-channels)
