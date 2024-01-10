5ddaf281a52096bbf5f3598de4a14389d6c654cf
Artyom Bologov <aartaka@protonmail.com>
2024-01-10 17:50:35 +0400

    channels: Remove guixrus channel.

--- channels.scm
+++ channels.scm
@@ -1,13 +1,5 @@
 (cons*
  (channel
-  (name 'guixrus)
-  (url "https://git.sr.ht/~whereiseveryone/guixrus")
-  (introduction
-   (make-channel-introduction
-    "7c67c3a9f299517bfc4ce8235628657898dd26b2"
-    (openpgp-fingerprint
-     "CD2D 5EAA A98C CB37 DA91  D6B0 5F58 1664 7F8B E551"))))
- (channel
   (name 'nonguix)
   (url "https://gitlab.com/nonguix/nonguix")
   ;; Enable signature verification:
